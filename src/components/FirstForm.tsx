f4754448b15778a32dfcdb69399cb4e2dcdb3028
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-04-05 17:20:03 +0200

    first form added

---
+++ src/components/FirstForm.tsx
@@ -0,0 +1,39 @@
+import { ChangeEvent, FormEvent, useState } from 'react'
+
+
+
+const FirstForm = () => {
+    const [color, setColor] = useState('')
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>)=> {
+        event.preventDefault()
+        
+    }
+    const handleColorChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const newColor = event.target.value;
+        if (newColor.length <= 7 && /^[a-fA-F0-9#]*$/.test(newColor)) {
+            setColor(newColor.replace(/[^a-fA-F0-9#]/g, '').toLowerCase());
+        }
+    }
+    const inputIsValid = (value: string): boolean => {
+        return /^#[a-fA-F0-9]{6}$/.test(value);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="colorInput" className="">Nazwa Koloru</label>
+            <input
+                id='colorInput' 
+                type="text" 
+                className="" 
+                placeholder='wpisz wybrany kolor'
+                value={color} 
+                onChange={handleColorChange} 
+                maxLength={7} 
+            />
+            <button type='submit'>Submit</button>
+        </form>
+    )
+}
+
+export default FirstForm
---
+++ src/components/SecondForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SecondForm = (props: Props) => {
+  return (
+    <div>secondForm</div>
+  )
+}
+
+export default SecondForm
