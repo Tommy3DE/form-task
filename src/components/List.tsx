200a4d56da8d84c0839f831b41c0d50e37f20386
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-04-08 20:40:35 +0200

    color sorting mechanics added

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,59 +1,76 @@
-import { Dispatch, SetStateAction } from 'react'
-import './list.scss'
-import { initialColors } from '../App'
+import { Dispatch, SetStateAction } from 'react';
+import './list.scss';
+import { initialColors } from '../App';
+
 type ListProps = {
   colorList: string[],
   setColorList: Dispatch<SetStateAction<string[]>>;
-}
+};
 
 const List = ({ colorList, setColorList }: ListProps) => {
   const handleDelete = (index: number, color: string) => {
     const newList = [...colorList];
-
     newList.splice(index, 1);
 
-
-    localStorage.getItem('colors')
-    const getLocalStorage: string | null = localStorage.getItem('colors')
-    const parsedLocalStorage: string[] = getLocalStorage ? JSON.parse(getLocalStorage) : []
+    localStorage.getItem('colors');
+    const getLocalStorage: string | null = localStorage.getItem('colors');
+    const parsedLocalStorage: string[] = getLocalStorage ? JSON.parse(getLocalStorage) : [];
 
     if (getLocalStorage) {
-      const excludedColor = parsedLocalStorage.filter((item) => item !== color)
-      localStorage.setItem('colors', JSON.stringify(excludedColor))
+      const excludedColor = parsedLocalStorage.filter((item) => item !== color);
+      localStorage.setItem('colors', JSON.stringify(excludedColor));
     }
     setColorList(newList);
-  }
+  };
 
   const hexToRGB = (hex: string) => {
-    hex = hex.replace('#', '')
-    const r = parseInt(hex.substring(0, 2), 16)
-    const g = parseInt(hex.substring(2, 4), 16)
-    const b = parseInt(hex.substring(4, 6), 16)
+    hex = hex.replace('#', '');
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
     return 'RGB(' + r + ', ' + g + ', ' + b + ')';
-  }
+  };
+
+  const getRGBValues = (color: string) => {
+    const hex = color.replace('#', '');
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+    return [r, g, b];
+  };
+
+  const sortedColorList = [...colorList].sort((colorA, colorB) => {
+    const [rA, gA, bA] = getRGBValues(colorA);
+    const [rB, gB, bB] = getRGBValues(colorB);
+    if (rA !== rB) {
+      return rB - rA;
+    } else if (gA !== gB) {
+      return gB - gA;
+    } else {
+      return bB - bA;
+    }
+  });
 
   return (
     <div>
       <ul className='goonline_list-ul'>
-        {colorList.map((color, index) => (
+        {sortedColorList.map((color, index) => (
           <li key={index}>
             <div className='goonline_list-ul-content'>
               <p>
                 <input type='color' value={color} disabled={true} />
                 {color.toUpperCase()}
               </p>
-              <p>
-                {hexToRGB(color)}
-              </p>
-              {!initialColors.includes(color) && <button onClick={() => handleDelete(index, color)}>
-                X
-              </button>}
+              <p>{hexToRGB(color)}</p>
+              {!initialColors.includes(color) && (
+                <button onClick={() => handleDelete(index, color)}>X</button>
+              )}
             </div>
           </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
